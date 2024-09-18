68362f8306bb529511450237e9fd72848ac29cca
Valera <108022376+kekobka@users.noreply.github.com>
2024-09-19 00:36:50 +0700

    реализовал простейший селектор (переделать)

--- code/Car/CarSelector.cs
+++ code/Car/CarSelector.cs
@@ -3,18 +3,35 @@ namespace DM.Car;
 
 public sealed class CarSelector : Component
 {
+	[Property] public GameObject ActiveCar { get; set; }
 	[Property] public List<GameObject> Cars { get; set; }
+	[Property] public int car { get; set; }
 
 	protected override void OnStart()
 	{
 		base.OnStart();
+		ChangeCar( Cars.First() );
+	}
 
-		var car = Cars.First();
+	protected override void OnUpdate()
+	{
+		if ( Input.Pressed( "Left" ) )
+		{
+			car = (car + 1) % Cars.Count;
+			ChangeCar( Cars[car] );
+		}
+	}
 
-		car = car.Clone();
+	private void ChangeCar( GameObject newCar )
+	{
+		ActiveCar?.Destroy();
+
+		GameObject car = newCar.Clone();
+		car.Name = "Car";
 		car.Transform.Position = Transform.Position;
 		car.Components.Get<Rigidbody>().MotionEnabled = false;
 		car.Components.Get<Car>().Enabled = false;
+		ActiveCar = car;
 	}
 
 }

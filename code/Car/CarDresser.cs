9a5bfa30aefa6b64b7d90191492279f00ee0ea79
Valera <108022376+kekobka@users.noreply.github.com>
2024-09-19 00:26:19 +0700

    full remove network in garage and unused files

--- code/Car/CarDresser.cs
+++ code/Car/CarDresser.cs
@@ -27,4 +27,25 @@ public sealed class CarDresser : Component, Component.INetworkSpawn
 
 		}
 	}
+	protected override void OnStart()
+	{
+		if ( IsProxy ) return;
+		int partid = 0;
+		Material mat = BodyRenderer.MaterialOverride;
+
+		foreach ( GameObject pos in Poses )
+		{
+			PartsList parts = pos.Components.Get<PartsList>();
+			if ( parts.Parts[partid] is null )
+				continue;
+			GameObject part = new( true, "Model" )
+			{
+				Parent = pos
+			};
+			var partrenderer = part.Components.Create<ModelRenderer>();
+			partrenderer.Model = parts.Parts[partid];
+			partrenderer.MaterialOverride = mat;
+
+		}
+	}
 }
---
+++ code/Car/CarSelector.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DM.Car;
+
+public sealed class CarSelector : Component
+{
+	[Property] public List<GameObject> Cars { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		var car = Cars.First();
+
+		car = car.Clone();
+		car.Transform.Position = Transform.Position;
+		car.Components.Get<Rigidbody>().MotionEnabled = false;
+		car.Components.Get<Car>().Enabled = false;
+	}
+
+}

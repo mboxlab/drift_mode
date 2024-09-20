813d3961bd9faa7b058c227da1dfc1d0763a3b9d
friztailo <friztailo@gmail.com>
2024-09-20 15:41:08 +0500

    Improvement interactive camera

--- code/Car/CarSelector.cs
+++ code/Car/CarSelector.cs
@@ -1,20 +1,30 @@
 ﻿
 namespace DM.Car;
+using DM.UI;
 
 public sealed class CarSelector : Component
 {
+	[Property] public GameObject Lift { get; set; }
 	[Property] public GameObject ActiveCar { get; set; }
+	[Property] public BBox ActiveCarBounds { get; set; }
 	[Property] public List<GameObject> Cars { get; set; }
 	[Property] public int car { get; set; }
+
+	private InteractiveCamera InteractiveCamera;
 
 	protected override void OnStart()
 	{
 		base.OnStart();
+
+		InteractiveCamera = Scene.GetAllComponents<InteractiveCamera>().First();
+
 		ChangeCar( Cars.First() );
 	}
 
 	protected override void OnUpdate()
 	{
+		if ( !InteractiveCamera.IsLooking ) return;
+
 		if ( Input.Pressed( "Left" ) )
 		{
 			car = (car + 1) % Cars.Count;
@@ -35,5 +45,6 @@ public sealed class CarSelector : Component
 		car.Components.Get<Car>().Enabled = false;
 		car.Components.Get<CameraController>().Destroy();
 		ActiveCar = car;
+		ActiveCarBounds = car.GetBounds();
 	}
 }

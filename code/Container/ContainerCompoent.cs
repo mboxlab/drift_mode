3aac7e152af80c7374f02bdd12fe325c292a6a0e
Valera <108022376+kekobka@users.noreply.github.com>
2024-10-11 19:47:20 +0700

    add container

---
+++ code/Container/ContainerCompoent.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Sandbox.Container;
+
+public sealed class ContainerCompoent : Component
+{
+	[Property] public SkinnedModelRenderer ContainerRenderer { get; set; }
+	protected override void OnStart()
+	{
+		base.OnStart();
+	}
+}
